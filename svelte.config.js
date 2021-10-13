194bcdbae48048939847b3cf5d57f3c2b0626d60
Ben Altair <baltair@bahaistudies.ca>
2021-10-13 16:02:03 -0700

    Update `pages` to `entries` as per Kit API change
    Fixes #1 which is present on new clones of this repo.

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 			crawl: true,
 			enabled: true,
 			onError: 'continue',
-			pages: ['*'],
+			entries: ['*'],
 		},
 	},
 	extensions: [".svelte", ...mdsvexConfig.extensions],
